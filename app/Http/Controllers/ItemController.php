21549369d9feabebf8f07aaeb991f3012f71b40e
r1d3rzz <myintthwaykhine127@gmail.com>
2022-09-29 14:21:54 +0630

    create REST api

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreItemRequest;
+use App\Models\Item;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    public function index(){
+        $items = Item::latest()->get();
+
+        return response()->json([
+            'status' => true,
+            'items' => $items
+        ],200);
+    }
+
+    public function show($id){
+        $item = Item::find($id);
+
+        if($item){
+            return response()->json([
+                'status' => true,
+                'item' => $item->get()
+            ],200);
+        }
+
+        return "item not found!";
+    }
+
+    public function store(StoreItemRequest $request){
+        $item = Item::create($request->all());
+
+        return response()->json([
+            'status' => true,
+            'message'=> 'created Successfully!',
+            'item' => $item
+        ],200);
+    }
+
+    public function update($id,StoreItemRequest $request){
+        $item = Item::find($id);
+
+        if($item){
+            $item->update($request->all());
+
+            return response()->json([
+                'status' => true,
+                'message'=> 'updated Successfully!',
+                'item' => $item
+            ],200);
+        }
+
+        return "item not found!";
+    }
+
+    public function destroy($id){
+        $item = Item::find($id);
+
+        if($item){
+            $item->delete();
+
+            return response()->json([
+                'status' => true,
+                'message'=> 'destroyed Successfully!',
+            ],200);
+        }
+
+        return "item not found!";
+    }
+}
